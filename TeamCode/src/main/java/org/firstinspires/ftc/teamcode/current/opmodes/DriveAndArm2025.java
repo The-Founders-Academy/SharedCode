72396ec0899ba5614e520817bb852996b31736b7
ian <iannpanait@gmail.com>
2024-10-18 07:48:58 -0400

    Added commands to initialize() function within DriveAndArm2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/current/opmodes/DriveAndArm2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/current/opmodes/DriveAndArm2025.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.current.opmodes;
 
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.Subsystem;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
@@ -12,6 +13,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.current.commands.ArmCommand;
 import org.firstinspires.ftc.teamcode.current.commands.DriverRelativeDrive;
 import org.firstinspires.ftc.teamcode.current.commands.WristCommand;
 import org.firstinspires.ftc.teamcode.current.subsytems.Arm2025;
@@ -31,26 +33,42 @@ DriveAndArm2025 extends CommandOpMode {
     private CommandGamepad m_driver;
     private Arm2025 armSubsystem;
 
-    public String NotPressed;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize();
+
+        waitForStart();
+
+        // run the scheduler
+        while (!isStopRequested() && opModeIsActive()) {
+            CommandScheduler.getInstance().run();
+        }
+        reset();
+    }
 
     @Override
     public void initialize() {
         MecanumConfigs configs = new MecanumConfigs().runMode(MotorEx.RunMode.RawPower);
+
         m_mecanumDrive = new Mecanum2025(hardwareMap, configs, new Pose2d(0, 0, Rotation2d.fromDegrees(90)), BaseMecanumDrive.Alliance.RED);
+        armSubsystem = new Arm2025(hardwareMap);
+
         m_driver = new CommandGamepad(gamepad1, 0.2, 0.2);
         m_mecanumDrive.setDefaultCommand(new DriverRelativeDrive(m_mecanumDrive, m_driver));
 
+
+        m_driver.buttonA().whenPressed(new WristCommand(armSubsystem, WristCommand.WristPosition.FOLDED_OUT));
+        m_driver.buttonB().whenPressed(new WristCommand(armSubsystem, WristCommand.WristPosition.FOLDED_IN));
+
+        m_driver.dpadUp().whenPressed(new ArmCommand(armSubsystem, ArmCommand.ArmPosition.ARM_ATTACH_HANGING_HOOK));
+        m_driver.dpadDown().whenPressed(new ArmCommand(armSubsystem, ArmCommand.ArmPosition.ARM_COLLAPSED_INTO_ROBOT));
+
+
     }
 
-    public void execute() {
-        if(gamepad1.a) {
-            Command folded_in = new WristCommand(armSubsystem, WristCommand.WristPosition.FOLDED_IN);
-            schedule(folded_in);
-
-            NotPressed = "Pressed";
-        }
-        else {
-            NotPressed = "No longer Pressed";
-        }
+    public void reset() {
+        // Cleanup or reset code here, if necessary
     }
+
+
 }
